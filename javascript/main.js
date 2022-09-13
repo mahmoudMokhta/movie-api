700c14968224ef5c71eb53cdd8ef234c3aa00225
mahmoudMokhta <mailto:mahmoud66mokhtar@gmail.com>
2022-09-13 12:48:11 +0200

    theFirst

---
+++ javascript/main.js
@@ -0,0 +1,48 @@
+const URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bd551351d613eae72b5219e295facfeb&page1";
+
+fetch(URL)
+  .then((response) => response.json())
+  .then(response);
+
+// console.log(data)
+
+let input = document.querySelector(".inputSearch");
+let submit = document.querySelector(".submit");
+let cards = document.querySelector(".cards");
+// btn  submit
+submit.addEventListener("click", () => {
+  cards.innerHTML = "";
+  let inputSearch = input.value;
+  const myAPI = `https://api.themoviedb.org/3/search/movie?api_key=bd551351d613eae72b5219e295facfeb&query=${inputSearch}`;
+
+  fetch(myAPI)
+    .then((response) => response.json())
+    .then(response);
+});
+
+//  the function
+function response(data) {
+  let abc = data.results;
+
+  abc.map((e) => {
+    // console.log(e)
+    let img = e.poster_path;
+    let overview = e.overview;
+    let release_date = e.release_date;
+    let title = e.title;
+    content = `<div class="card ">
+                <div class="img">
+                <img src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/${img}">
+                <div class="down-img d-flex justify-content-between">
+                    <p class="one">${title}</p>
+                    <p class="two">${release_date}</p>
+                </div>
+            </div>
+            
+            <p class="overview"><span>mahmoud</span>${overview}</p>
+        </div>`;
+
+    cards.innerHTML += content;
+  });
+}
